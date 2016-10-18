61b5b0755af0c7cdbf36312e9ba0294d91572c02
Mary Miller <marymiller1954@gmail.com>
2016-10-18 18:40:59 -0400

    second commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,58 @@
 var friends = [
 	{
-		"name": "Cindy",
-		"photo": "https://s-media-cache-ak0.pinimg.com/736x/da/a5/d3/daa5d3b977b95f59a31e48ddfceabb8c.jpg",
+		"name": "Grace Slick",
+		"photo": "http://richestcelebrities.org/wp-content/uploads/2015/05/Grace-Slick-Net-Worth.jpgg",
 		"scores": [3, 2, 5, 1, 4, 2, 2, 4, 3, 1]
 	},
 	{
 		"name": "Unji",
-		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
+		"photo": "https://1.bp.blogspot.com/-1BdMPq0tGCc/V4Nlh__3cNI/AAAAAAAAMKY/WhWl9MXDhwIQcyzK0Cq7fWwClMEGPlarwCLcB/s320/1.jpg",
 		"scores": [3, 2, 1, 5, 3, 4, 2, 1, 4, 3]
 	},
 	{
-		"name": "Tommy",
-		"photo": "https://www.facebook.com/photo.php?fbid=10157441559565487&set=a.10150717139525487.707805.633755486&type=3",
+		"name": "Bob Dylan",
+		"photo": "http://art-sheep.com/wp-content/uploads/2015/05/dylan.jpg",
 		"scores": [3, 3, 1, 4, 5, 4, 2, 5, 4, 2]
 	},
 	{
-		"name": "Isla",
-		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
+		"name": "Kate Middleton",
+		"photo": "http://d.ibtimes.co.uk/en/full/1431780/kate-middleton.jpg?w=736",
 		"scores": [2, 2, 4, 5, 5, 4, 4, 1, 4, 4]
 	},
 	{
-		"name": "Louie",
-		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
+		"name": "Woody Allen",
+		"photo": "http://d1s6yijfrvmtp9.cloudfront.net/wp-content/uploads/2014/07/26164214/woody-allen.jpg",
 		"scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	},
 	{
-		"name": "Corinna",
-		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
+		"name": "Van Morrison",
+		"photo": "http://test-nodep.s3-us-west-2.amazonaws.com/VanMorrison.jpg",
 		"scores": [4, 5, 3, 2, 1, 4, 4, 5, 4, 5]
 	},
 	{
-		"name": "Janet",
-		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
+		"name": "Prince Harry",
+		"photo": "http://us.hellomagazine.com/imagenes/royalty/2016010829125/prince-harry-nepal-visit-confirmed/0-143-445/PrinceHarry--a.jpg",
 		"scores": [4, 3, 2, 1, 5, 4, 3, 2, 1, 3]
 	},
 	{
 		"name": "Queen Elizabeth",
 		"photo": "http://matzav.com/wp-content/uploads/2016/04/Queen-Elizabeth-II.jpg",
 		"scores": [4, 3, 2, 1, 5, 4, 3, 2, 1, 3]
+	},
+
+{
+		"name": "John Fogerty",
+		"photo": "https://pbs.twimg.com/profile_images/580023049035980800/g_BxZEQ7.jpg",
+		"scores": [4, 3, 2, 1, 5, 4, 3, 2, 1, 3]
+	},
+	{
+		"name": "Janis Joplin",
+		"photo": "http://pixel.nymag.com/imgs/daily/vulture/2015/11/16/16-janis-joplin.w529.h529.jpg",
+		"scores": [4, 3, 2, 1, 5, 4, 3, 2, 1, 3]
 	}
+
+
+
 ];
 
 module.exports = friends;
